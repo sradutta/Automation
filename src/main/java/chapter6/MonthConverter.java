019d1e6a9dfc50d381c0ce0b28cd65b8446a4b0d
Sra Dutta <sradutta@gmail.com>
2022-04-24 17:43:54 -0400

    java objects static and non-static another example

---
+++ src/main/java/chapter6/MonthConverter.java
@@ -0,0 +1,9 @@
+package chapter6;
+
+public class MonthConverter {
+    public static void main(String args[]){
+        System.out.println(Month.getMonth(5));
+        System.out.println(Month.getMonth("October")); //we can access them without instantiating because all are static
+
+    }
+}
